d877a3187c63e58e6b0b9cc54fa5909ec9d80eec
Anton Skrypnik <a.skrypnik@rollncode.com>
2018-07-25 11:58:40 +0300

    set error property as public

--- OBD2-Swift/Classes/Model/Response.swift
+++ OBD2-Swift/Classes/Model/Response.swift
@@ -19,7 +19,7 @@ public struct Response : Hashable, Equatable {
   var pid : UInt8 = 0
   var data : Data?
   var rawData : [UInt8] = []
-    var error: Error? = nil
+    public var error: Error? = nil
   
   public var strigDescriptor : String?
     public static let obdErrorDomain = "com.obd2-Swift.Error"
